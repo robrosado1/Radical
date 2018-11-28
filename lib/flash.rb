31ff5ef5d0bc5faa542925c0375bea234a601fa7
Robinson <robrosado1@gmail.com>
2018-11-28 12:26:33 -0500

    complete flash cookie

--- lib/flash.rb
+++ lib/flash.rb
@@ -1,4 +1,24 @@
 require 'json'
 
 class Flash
+  attr_reader :now
+
+  def initialize(req)
+    cookie = req.cookies['_rails_lite_app_flash']
+    @now = cookie.nil? ? {} : JSON.parse(cookie)
+    @flash_cookie = {}
+  end
+
+  def [](key)
+    @now[key.to_s] || @flash_cookie[key.to_s]
+  end
+
+  def []=(key, value)
+    @flash_cookie[key.to_s] = value
+  end
+
+  def store_flash(res)
+    res.set_cookie('_rails_lite_app_flash', path: '/', value: @flash_cookie.to_json)
+  end
+
 end
